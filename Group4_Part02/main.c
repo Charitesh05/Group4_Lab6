08e0766bdbab5211e0e6d312ee4687f86303f871
charitesh <chariteshcharitesh@gmail.com>
2024-09-22 23:34:55 +0530

    modified part2 main

--- Group4_Part02/main.c
+++ Group4_Part02/main.c
@@ -11,20 +11,25 @@
 #define CLKINT (1 << 2)      // bit 2 of CSR to specify CPU clock
 #define INTERRUPT (1 << 1)   // bit 2 of CSR to specify CPU clock
 #define CLOCK_MHZ 16
-
-volatile int f = 100000;
-volatile int percent = 5;
-volatile int x = (8 * 1000000) /100000;  // Fix exponentiation issue
+volatile uint32_t timerValue = 0;
+volatile int timerRunning = 0; // Flag to check if the timer is running
+volatile int f = 100000; // Frequency in Hz
+volatile int percent = 5; // Step size for duty cycle adjustment
+volatile int x = (8 * 1000000) / 100000;  // Initial duty cycle
 volatile int interrupt_count = 0;
 
 void GPIO_F0_INT_HANDLER(void) {
-    x = x + (160000 * percent) / f;  // Fixed exponentiation issue
-    x = ((16000000 / f)  < x) ? (16000000 / f) : x;
+    x = x + (160000 * percent) / f;  // Increase x
+    // Ensure x doesn't exceed the maximum value for a valid duty cycle
+    x = ((16000000 / f) <= x) ? (16000000 / f) : x;
+
 }
 
 void GPIO_F4_INT_HANDLER(void) {
-    x = x - (160000 * percent) / f;  // Fixed exponentiation issue
-    x = (x < 0) ? 0 : x;
+    x = x - (160000 * percent) / f;  // Decrease x
+    // Ensure x doesn't go below 1
+    x = (x <= 0) ? 0 : x;
+
 }
 
 void GPIO_INIT(void) {
@@ -36,24 +41,39 @@ void GPIO_INIT(void) {
     GPIO_PORTF_PUR_R = 0x11;            /* PORTF4 is pulled up */
 }
 
+void Timer0A_Init(void) {
+    SYSCTL_RCGCTIMER_R |= 0x01;       // Enable clock for Timer0
+    TIMER0_CTL_R &= ~0x01;            // Disable Timer0A during configuration
+    TIMER0_CFG_R = 0x00;              // Configure for 32-bit timer mode (concatenated)
+    TIMER0_TAMR_R = 0x12;             // TAMR: Up-counting, periodic mode
+    TIMER0_TAILR_R = 0xFFFFFFFF;      // Set interval load to max (for long counting)
+    TIMER0_ICR_R = 0x01;              // Clear the timeout flag
+    TIMER0_IMR_R &= ~0x01;            // Disable timer interrupts (not needed for this case)
+}
+
 void GPIO_INT_HANDLER(void) {
-    int z=0;
+    GPIO_PORTF_ICR_R = 0x10; // Clear interrupt flags
     interrupt_count = interrupt_count + 1;
-    int temp_x = 500;
+    int temp_x = 5;
     while (temp_x > 0) {
         temp_x = temp_x - 1;
     }
-    while(!(GPIO_PORTF_DATA_R & 0x10)){
-        z=z+1;
+    if (timerRunning == 0) {
+        // Start Timer
+        TIMER0_CTL_R |= 0x01;     // Enable Timer0A
+        timerRunning = 1;
+    } else {
+        // Stop Timer
+        TIMER0_CTL_R &= ~0x01;    // Disable Timer0A
+        timerValue = TIMER0_TAR_R;  // Read current timer value
+        TIMER0_TAV_R = 0;         // Reset Timer0A counter to 0
+        timerRunning = 0;
+        if (timerValue < 6500000) {
+            GPIO_F0_INT_HANDLER(); // Call GPIO_F0 handler
+        } else {
+            GPIO_F4_INT_HANDLER(); // Call GPIO_F4 handler
+        }
     }
-    if (z>320000) {
-        GPIO_F4_INT_HANDLER(); // Call GPIO_F4 handler
-    }
-    if (z<=320000) {
-        GPIO_F0_INT_HANDLER(); // Call GPIO_F0 handler
-    }
-
-    GPIO_PORTF_ICR_R = 0x11; // Clear interrupt flags
 }
 
 void Delay(int us) {
@@ -70,20 +90,22 @@ void Delay(int us) {
 
 int main(void) {
     GPIO_INIT();                      // Initialize GPIO
+    Timer0A_Init();
     GPIO_PORTF_IS_R = 0x00;           // Edge-sensitive interrupts
-    GPIO_PORTF_IBE_R = 0x00;          // Interrupt on single edge
-    GPIO_PORTF_IEV_R = 0x00;          // Falling edge triggers interrupt
-    GPIO_PORTF_IM_R = 0x11;           // Unmask interrupts for PF4 and PF0
+    GPIO_PORTF_IBE_R = 0x10;          // Interrupt on single edge
+    GPIO_PORTF_IM_R = 0x10;           // Unmask interrupts for PF4 and PF0
     NVIC_EN0_R = 0x40000000;          // Enable IRQ30 (GPIO Port F interrupt)
 
     while (1) {
-        if(x!=0){
-        GPIO_PORTF_DATA_R |= 0x02;    // Turn on LED connected to PF1
-        Delay(x);                     // Delay depending on the value of x
+
+        if (x != 0 ) {
+            GPIO_PORTF_DATA_R |= 0x02;    // Turn on LED connected to PF1
+            Delay(x);                     // Delay depending on the value of x
         }
-        if(x!=(16000000 / f)){
-        GPIO_PORTF_DATA_R &= ~0x02;   // Turn off LED connected to PF1
-        Delay((16000000 / f) - x);    // Fixed exponentiation issue
+       if(x != (16000000 / f)){
+            GPIO_PORTF_DATA_R &= ~0x02;   // Turn off LED connected to PF1
+            Delay((16000000 / f) - x);    // Delay for the remaining time
         }
     }
-}
+    }
+
